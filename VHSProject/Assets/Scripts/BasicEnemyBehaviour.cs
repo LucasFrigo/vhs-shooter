63f8edad115126d7e1edfa08920e340c1a7812fc
Lucas Guilherme Frigo <lukeg299@gmail.com>
2019-08-14 11:18:09 -0300

    added basic enemy behaviour

---
+++ VHSProject/Assets/Scripts/BasicEnemyBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+    public GameObject moveTo;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        InvokeRepeating("ChangeXPosition", 2.0f, 4.0f);
+    }
+
+    void FixedUpdate()
+    {
+        //transform.position = Vector3.MoveTowards(transform.position, moveTo.transform.position, speed * Time.deltaTime);
+        
+       
+
+    }
+
+    void ChangeXPosition()
+    {
+        if (!sprite.flipX)
+        {
+            sprite.flipX = true;
+            Debug.Log("Changed X pos to true");
+        }
+        else
+        {
+            sprite.flipX = false;
+            Debug.Log("Changed X pos to false");
+        }
+    }
+}
