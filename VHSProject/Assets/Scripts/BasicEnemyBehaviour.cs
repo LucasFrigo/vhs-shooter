5f62dda9602b5c053206cb97904dc42c3eca2c27
Lucas Guilherme Frigo <lukeg299@gmail.com>
2019-08-19 11:00:02 -0300

    added projectile shooting mechaninc

--- VHSProject/Assets/Scripts/BasicEnemyBehaviour.cs
+++ VHSProject/Assets/Scripts/BasicEnemyBehaviour.cs
@@ -8,16 +8,26 @@ public class BasicEnemyBehaviour : MonoBehaviour
     private float speed = 10f;
     public GameObject moveTo;
     private SpriteRenderer sprite;
+    public GameObject projectile;
+    private GameObject travellingProj;
+    
 
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         InvokeRepeating("ChangeXPosition", 2.0f, 0.5f);
+        InvokeRepeating("ShootProjectile", 1.0f, 2.0f);
+        
+        
     }
 
     void FixedUpdate()
     {
         transform.position = Vector3.MoveTowards(transform.position, moveTo.transform.position, speed * Time.deltaTime);
+        var dir = moveTo.transform.position - transform.position;
+        dir = dir.normalized;
+        travellingProj.GetComponent<Rigidbody2D>().AddForce(dir * 10);
+
     }
 
     void ChangeXPosition()
@@ -32,5 +42,11 @@ public class BasicEnemyBehaviour : MonoBehaviour
             sprite.flipX = false;
             Debug.Log("Changed X pos to false");
         }
+    }
+
+    void ShootProjectile()
+    {
+        travellingProj = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        
     }
 }
